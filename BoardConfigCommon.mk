f91438ecc9e6fdf350e9a030fbc4988620644c1a
C457 <android.c357@gmail.com>
2014-08-25 20:12:20 -0500

    shieldtablet: wifi /system > /vendor fix

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -70,7 +70,7 @@ BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
 BOARD_WLAN_DEVICE           := bcmdhd
 BOARD_HOSTAPD_DRIVER        := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_bcmdhd
-WIFI_DRIVER_FW_PATH_STA     := "/system/vendor/firmware/43241/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_STA     := "/vendor/firmware/43241/fw_bcmdhd.bin"
 WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
 WIFI_DRIVER_MODULE_ARG      := "iface_name=wlan0"
 WIFI_DRIVER_MODULE_NAME     := "bcmdhd"
